b18cccd5db4141bde1dbf5b8f26a387051b67d41
Ryan Fitzgerald <ryan@codebrewstudios.com>
2012-02-23 16:21:55 -0500

    throwing an error if given compiler isnt installed

--- examples/simple.js
+++ examples/simple.js
@@ -2,6 +2,9 @@ var assembly = require("../lib/assembly");
 
 var opts = {src : __dirname + "/src", out: __dirname +"/build"};
 
-assembly.start(opts);
+var assembler = assembly.start(opts);
+assembler.use(assembly.compilers.coffee);
+assembler.use(assembly.compilers.less);
+assembler.use(assembly.compilers.hbs);
 
 console.log("monitoring dir:", opts.src);
