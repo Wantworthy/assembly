438dfea45f3e8ec54b365b161500b2abee2f20b6
Ryan Fitzgerald <ryan@codebrewstudios.com>
2012-02-23 18:22:18 -0500

    making all compilers optional, loading them all up and still letting them be configured if wanted

--- examples/simple.js
+++ examples/simple.js
@@ -3,9 +3,6 @@ var assembly = require("../lib/assembly");
 var opts = {src : __dirname + "/src", out: __dirname +"/build"};
 
 var assembler = assembly.start(opts);
-assembler.use(assembly.compilers.coffee);
-assembler.use(assembly.compilers.less);
-assembler.use(assembly.compilers.hbs);
 
 console.log("monitoring dir:", opts.src);
 assembler.rebuild();
