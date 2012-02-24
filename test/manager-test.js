7c4fb62e3807a84d7bd202f0d05323de89e7e25c
Ryan Fitzgerald <ryan@codebrewstudios.com>
2012-02-24 18:13:36 -0500

    making manager be a broadway app, only compiling a file if it has been modified

---
+++ test/manager-test.js
@@ -0,0 +1,15 @@
+var Manager = require("../lib/assembly/manager").Manager,
+    util = require('utile'),
+    should = require('should');
+
+describe("Assembly Manager", function() {
+
+  before(function(done){
+    done();
+  });
+
+  it('should make request to update item', function(done) {
+    done();
+  });
+
+});
