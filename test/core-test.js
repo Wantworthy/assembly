cbff000ee7bf0fb44205cdb8a7bf819ab0b462cc
Ryan Fitzgerald <ryan@codebrewstudios.com>
2012-02-27 14:42:32 -0500

    adding requires lookup

--- test/core-test.js
+++ test/core-test.js
@@ -1,7 +1,8 @@
 var Core = require("../lib/assembly/core").Core,
     util = require('utile'),
     should = require('should'),
-    fs = require("fs");
+    fs = require("fs"),
+    path = require('path');
 
 describe("Core", function() {
   var core;
@@ -13,13 +14,13 @@ describe("Core", function() {
   describe('Build Path', function() {
 
     it("should return relative vendor path for libs in node_modules", function(){
-      var path = core.buildPath("/home/user/code/node/assembly/node_modules/underscore/underscore.js");
-      path.should.eql("vendor/underscore/underscore.js");
+      var dest = core.buildPath("/home/user/code/node/assembly/node_modules/underscore/underscore.js");
+      dest.should.eql("vendor/underscore/underscore.js");
     });
 
     it("should return absolute vendor path for libs in node_modules", function(){
-      var path = core.buildPath("/home/user/code/node/assembly/node_modules/underscore/underscore.js", {fullpath: true});
-      path.should.eql(core.dest + "/vendor/underscore/underscore.js");
+      var dest = core.buildPath("/home/user/code/node/assembly/node_modules/underscore/underscore.js", {fullpath: true});
+      dest.should.eql(core.dest + "/vendor/underscore/underscore.js");
     });
 
     it('should return relative build/vendor path for src/vendor lib', function() {
@@ -40,6 +41,25 @@ describe("Core", function() {
     it('should return absolute dest path for src file', function() {
       var dest = core.buildPath(core.src +"/foo.js", {fullpath : true});
       dest.should.eql(core.dest + "/foo.js");
+    });
+
+  });
+
+  describe('Requires', function(){
+    
+    it("should return single relative require", function() {
+      var requires = core.requires(fs.readFileSync(core.src+ "/app.js"));
+      requires.should.have.length(1);
+      requires.should.eql([core.src + "/foo.js"]);
+    });
+
+    it("should return requires from relative, vendor and node_modules", function() {
+      var requires = core.requires(fs.readFileSync(core.src+ "/complex.js"));
+      requires.should.have.length(3);
+
+      requires.should.include(core.src + "/foo.js");
+      requires.should.include(path.resolve(__dirname + "/../vendor", "handlebars.runtime.js"));
+      requires.should.include(require.resolve('underscore'));
     });
 
   });
