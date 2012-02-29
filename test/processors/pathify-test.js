b4e008681b7199e52e06f8a455c42e5aeb448bf6
Ryan Fitzgerald <ryan@codebrewstudios.com>
2012-02-29 12:01:55 -0500

    creating processor to update require paths to full destination paths

---
+++ test/processors/pathify-test.js
@@ -0,0 +1,57 @@
+var Pathify = require("../../lib/assembly/processors/pathify").Pathify,
+    assembly = require("../../lib/assembly"),
+    TestHelper = require("../test-helper"),
+    util = require('utile'),
+    should = require('should'),
+    fs = require("fs"),
+    path = require('path');
+
+describe("Pathify Processor", function() {
+  var core = TestHelper.core,
+      pathify;
+
+  beforeEach(function(){
+    pathify = new Pathify(core);
+  });
+
+  it("should update require for relative require at base path", function(){
+    var out = pathify.process(core.src +"/foo.js", 'var app = require("./app");');
+
+    out.should.equal('var app = require("app");');
+  });
+
+  it("should update require for lib ", function(){
+    var out = pathify.process(core.src +"/foo.js", 'var app = require("handlebars.runtime");');
+
+    out.should.equal('var app = require("vendor/handlebars.runtime");');
+  });
+
+  it("should update require for sibling and parent require ", function(){
+    var out = pathify.process(core.src +"/nested/foo.js", 'var app = require("../app"), baz = require("./baz");');
+
+    out.should.equal('var app = require("app"), baz = require("nested/baz");');
+  });
+
+  it("should update require for nested require ", function() {
+    var out = pathify.process(core.src +"/foo.js", 'var baz = require("./nested/baz")');
+
+    out.should.equal('var baz = require("nested/baz");');
+  });
+
+  it("should not do anything when require is already at the correct full path ", function() {
+    var out = pathify.process(core.src +"/foo.js", 'var app = require("app")');
+
+    out.should.equal('var app = require("app");');
+  });  
+
+  it("should handle dot requires", function() {
+    var out = pathify.process(core.src +"/foo.js", 'var name = require("./nested/baz").name');
+    out.should.equal('var name = require("nested/baz").name;');
+  });
+
+  it("should handle crazyness", function() {
+    var out = pathify.process(core.src +"/foo.js", '(function () {require("./nested/baz");})();');
+    out.should.equal('(function() {\n    require("nested/baz");\n})();');
+  });
+
+});
