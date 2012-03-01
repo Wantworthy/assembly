8bbc5d21106ed6b17ae75114f3af3eb28ddee989
Ryan Fitzgerald <ryan@codebrewstudios.com>
2012-02-29 19:48:23 -0500

    adding pulling in backbone to example

--- examples/src/app.js
+++ examples/src/app.js
@@ -1,6 +1,9 @@
 var name = require("./foo").name;
 
+var backbone = require('backbone');
+
 console.log("foo's name is.....", name);
+console.log("backbone version is", backbone.VERSION);
 exports.app = {
   foo : "moar...",
   name : name
