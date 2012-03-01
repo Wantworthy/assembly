83ffc8ab95bbd5bd00ac1afefacffa110104a674
Ryan Fitzgerald <ryan@codebrewstudios.com>
2012-03-01 17:53:06 -0500

    updating example to use jst template

--- examples/src/app.js
+++ examples/src/app.js
@@ -1,10 +1,13 @@
 var $ = require('jquery'),
     backbone = require('backbone'),
+    template = require('templates/world'),
     name = require("./foo").name;
 
 console.log("foo's name is.....", name);
 console.log("backbone version is", backbone.VERSION);
+
 exports.app = {
   foo : "moar...",
-  name : name
+  name : name,
+  template : template
 };
