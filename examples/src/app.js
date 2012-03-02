b3449e6ab9c875c12565ce29b39fee5485447a46
Ryan Fitzgerald <ryan@codebrewstudios.com>
2012-03-01 19:22:25 -0500

    adding handlebars example to app

--- examples/src/app.js
+++ examples/src/app.js
@@ -1,6 +1,7 @@
 var $ = require('jquery'),
     backbone = require('backbone'),
     template = require('templates/world'),
+    hbarsTemplate = require('templates/hello'),
     name = require("./foo").name;
 
 console.log("foo's name is.....", name);
@@ -9,5 +10,6 @@ console.log("backbone version is", backbone.VERSION);
 exports.app = {
   foo : "moar...",
   name : name,
-  template : template
+  template : template,
+  hbarsTemplate : hbarsTemplate
 };
