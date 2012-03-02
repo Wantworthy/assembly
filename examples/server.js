5d7db25c45ee5b52127036155921ee46c9b141ac
Ryan Fitzgerald <ryan@codebrewstudios.com>
2012-03-02 13:51:00 -0500

    adding command line support

--- examples/server.js
+++ examples/server.js
@@ -2,13 +2,12 @@ var assembly = require("../lib/assembly"),
     express = require('express'),
     server = express.createServer();
 
-var assembler = assembly.start({src : __dirname + "/src", dest: __dirname +"/build"});
+var assembler = assembly.createAssembler({src : __dirname + "/src", dest: __dirname +"/build"});
 assembler.use(assembly.processors.amdify);
 assembler.use(assembly.processors.pathify);
+assembler.start();
 
 server.use(express.static(assembler.dest));
 server.listen(8080, '0.0.0.0', function() {
   console.log("starting up server on localhost:8080");
 });
-
-assembler.rebuild();
