ac2e4920e276f0d5c73eb275841a495e2f701678
Ryan Fitzgerald <ryan@codebrewstudios.com>
2012-03-08 14:22:56 -0500

    refactoring less compiler plugin to use a compiler class

--- test/compilers/less-test.js
+++ test/compilers/less-test.js
@@ -1,19 +1,24 @@
 var TestHelper = require("../test-helper"),
-    lesscompiler = require("../../lib/assembly/compilers/less"),
+    LessCompiler = require("../../lib/assembly/compilers/less").LessCompiler,
     util = require('util'),
     should = require('should'),
     fs = require("fs"),
     path = require('path');
 
 describe("Less Compiler", function() {
+  var compiler;
+  beforeEach(function(){
+    compiler = new LessCompiler({}, {});
+  });
+
   it('should compile simple less file', function() {
-    lesscompiler.compile(TestHelper.testSrcDir + "/less/style.less", TestHelper.fixture("/less/style.less"), function(err, data) {
+    compiler.compile(TestHelper.testSrcDir + "/less/style.less", TestHelper.fixture("/less/style.less"), function(err, data) {
       data.should.equal(TestHelper.fixture("/less/style.css"));
     });
   });
 
   it('should handle relative import', function() {
-    lesscompiler.compile(TestHelper.testSrcDir + "/less/import.less", TestHelper.fixture("/less/import.less"), function(err, data) {
+    compiler.compile(TestHelper.testSrcDir + "/less/import.less", TestHelper.fixture("/less/import.less"), function(err, data) {
       data.should.equal(TestHelper.fixture("/less/style.css"));
     });
   });
