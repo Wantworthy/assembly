21fbb5edf18360659e6908d3dc2b72578c912539
Ryan Fitzgerald <ryan@codebrewstudios.com>
2012-03-18 20:15:21 -0400

    removing simple example

--- examples/simple.js
+++
@@ -1,9 +0,0 @@
-var assembly = require("../lib/assembly");
-
-var opts = {src : __dirname + "/src", dest: __dirname +"/build"};
-
-var assembler = assembly.createAssembler({src : __dirname + "/src", dest: __dirname +"/build"});
-assembler.use(assembly.processors.amdify);
-assembler.start();
-
-console.log("monitoring dir:", opts.src);
