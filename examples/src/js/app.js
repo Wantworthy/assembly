baef504fb78298f21b958b2a1ac451566ae7a52b
Derek Dahmer <derekdahmer@gmail.com>
2012-04-19 18:03:29 -0400

    Compiler for converting json files to a require-able JS files. Only the dictionary keyed by the current environment is taken. closes #15

--- examples/src/js/app.js
+++ examples/src/js/app.js
@@ -2,10 +2,12 @@ var $ = require('jquery'),
     backbone = require('backbone'),
     template = require('templates/world'),
     hbarsTemplate = require('templates/hello'),
-    name = require("./foo").name;
+    name = require("./foo").name,
+    myconfig = require("./myconfig");
 
 console.log("foo's name is.....", name);
 console.log("backbone version is", backbone.VERSION);
+console.log("Config variable some_key: " + myconfig.some_key);
 
 exports.app = {
   foo : "moar...",
