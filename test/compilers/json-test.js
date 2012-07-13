4be8a73cdafea38c53d36d15707e70b170c941d9
Ryan Fitzgerald <ryan@codebrewstudios.com>
2012-07-13 17:26:40 -0400

    removing environment variable config from json compiler

--- test/compilers/json-test.js
+++ test/compilers/json-test.js
@@ -9,18 +9,15 @@ describe("JsonCompiler", function() {
   var compiler;
   beforeEach(function(){
     var app = TestHelper.mockApp();
-    compiler = new JsonCompiler({env:'test'});
+    compiler = new JsonCompiler();
   });
 
-  it('should have a working beforeEach', function(done) {
-    done();
-  });
   describe("Compile", function(){
     it('should only have dev variables', function(done) {
       compiler.compile(TestHelper.jsRoot + "/config.json", TestHelper.fixture("/js/config.json"), function(err, data) {
-        if (err) { throw err }
+        should.not.exist(err);
+
         data.should.include('def');
-        data.should.not.include('abc');        
         done();
       });
     });
